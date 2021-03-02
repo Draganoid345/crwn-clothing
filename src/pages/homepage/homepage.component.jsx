cb4c95f4f377d98020f6d46b36db8dc134ac6947
Dragos Rusnac <dragos.rusnac@gmail.com>
2021-03-02 15:11:42 +0100

    added components and pages folder

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Direcotry from "../../components/directory/directory.component";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Direcotry />
+  </div>
+);
+
+export default HomePage;
